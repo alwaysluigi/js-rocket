262f3a2ff95db73452e44e41a95494ef3ad0aec2
alwaysluigi <alwaysluigi@users.noreply.github.com>
2020-10-26 15:23:10 -0500

    more work on rocket launch

--- js/rocket.js
+++ js/rocket.js
@@ -10,11 +10,29 @@ var changeState = function (state) {
 		document.getElementById('countdown').
 		innerHTML = countdownNumber;
 		countdownNumber = countdownNumber -1;
+		
+		if (countdownNumber > 4 && countdownNumber <=7) {
+			// excited
+			document.getElementById('excited').className = "excited show";
+				}else {
+					document.getElementById('excited').className = "excited";
+				}
+
+		if (countdownNumber > 1 && countdownNumber <=4 ) {
+			//can't wait
+	document.getElementById('wee').className = "wee show";
+	}
+		else {
+			document.getElementById('wee').className = "wee";
+		}
+	{
+		};
+
 		if (countdownNumber <= 0){
 
-			changeState(3);
+		changeState(3);
 		};
-		}, 100);	
+		}, 400);	
 	} else if (state == 3){
 		var success = setTimeout(function ()
 		{
